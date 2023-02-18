01c82dfa4653d8cf7724f8c5fe322d58ac112266
boywonder3 <opeyemiisaacboywonder@gmail.com>
2023-02-19 00:22:37 +0100

    fourth commit

--- Calculator.js
+++ Calculator.js
@@ -7,17 +7,56 @@ export default class Calculator {
         this.clear()
     }
 
+    get primaryOperand() {
+        return parseFloat(this.primaryOperandDisplay.dataset.value)
+    }
+    set primaryOperand(value) {
+        this.primaryOperandDisplay.dataset.value = value ?? ''
+        this.primaryOperandDisplay.textContent = displayNumber (value)
+    }
+
+    set secondaryOperand(value) {
+        this.secondaryOperandDisplay.textContent = displayNumber ?? ''
+        this.secondaryOperandDisplay.textContent = displayNumber (value)
+    }
+    set operation(value) {
+        this.operationDisplay.textContent = value ?? ''
+    }
+
     addDigit(digit) {
-        if(this.primaryOperandDisplay.textContent === '0') {
-            this.primaryOperandDisplay.textContent = digit
-            return
+        if (digit === '.' && this.primaryOperandDisplay.dataset.value.includes('.')) {
+            return 
         }
-        this.primaryOperandDisplay.textContent += digit
+        if (this.primaryOperand === 0) {
+            this.primaryOperand = digit
+            return 
+        }
+        this.primaryOperand = this.primaryOperandDisplay.dataset.value + digit
+    }
+
+    removeDigit() {
+        const numberString = this.primaryOperandDisplay.dataset.value
+        if(numberString.length <= 1) {
+            this.primaryOperand = 0
+            return 
+        }
+        this.primaryOperand = numberString.substring(0, numberString.length - 1)
     }
 
     clear() {
-        this.primaryOperandDisplay.textContent = 0
-        this.secondaryOperandDisplay.textContent = ''
-        this.operationDisplay.textContent = ''
+        this.primaryOperand = 0
+        this.secondaryOperand = null
+        this.operation = null
     }
+}
+
+const NUMBER_FORMATTER = new Intl.NumberFormat('en')
+
+function displayNumber(number) {
+    const stringNumber = number?.toString() || ''
+    if (stringNumber === '') return ''
+    const [integer, decimal] = stringNumber.split('.')
+    const formattedInteger = NUMBER_FORMATTER.format(integer)
+    if (decimal == null) return formattedInteger
+    return NUMBER_FORMATTER.format(number)
 }
