b881a81bc935186c7d2fc417a92b7327d3b1c800
boywonder3 <opeyemiisaacboywonder@gmail.com>
2023-02-18 22:52:31 +0100

    third commit

--- Calculator.js
+++ Calculator.js
@@ -1,3 +1,23 @@
 export default class Calculator {
-    
+    constructor(primaryOperandDisplay, secondaryOperandDisplay, operationDisplay) {
+        this.primaryOperandDisplay = primaryOperandDisplay
+        this.secondaryOperandDisplay = secondaryOperandDisplay
+        this.operationDisplay = operationDisplay
+
+        this.clear()
+    }
+
+    addDigit(digit) {
+        if(this.primaryOperandDisplay.textContent === '0') {
+            this.primaryOperandDisplay.textContent = digit
+            return
+        }
+        this.primaryOperandDisplay.textContent += digit
+    }
+
+    clear() {
+        this.primaryOperandDisplay.textContent = 0
+        this.secondaryOperandDisplay.textContent = ''
+        this.operationDisplay.textContent = ''
+    }
 }
--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 import Calculator from './Calculator.js'
 
+const primaryOperandDisplay = document.querySelector('[data-primary-operand]')
+const secondaryOperandDisplay = document.querySelector('[data-secondary-operand]')
+const operationDisplay = document.querySelector('[data-operation]')
+
 // ACTIONS 
 // 1. CLICK A NUMBER 
 // 2. CLICK CLEAR BUTTON
@@ -8,17 +12,25 @@ import Calculator from './Calculator.js'
 // 5. CLICK THE PERIOD BUTTON
 // 6. CLICK EQUALS 
 
-const calculator = new calculator()
+const calculator = new Calculator(
+    primaryOperandDisplay, 
+    secondaryOperandDisplay,
+     operationDisplay)
 
-document.addEventListener('', e => {
+document.addEventListener('click', e => {
     if (e.target.matches('[data-all-clear]')) {
         calculator.clear()
     }
-
     if (e.target.matches('[data-number]')) {
         calculator.addDigit(e.target.textConten)
     }
     if (e.target.matches('[data-delete]')) {
-        calculator.delete(e.target.textConten)
+        calculator.removeDigit()
+    }
+    if (e.target.matches('[data-operation]')) {
+        calculator.chooseOperation(e.target.tectContent)
+    }
+    if (e.target.matches('[data-equals]')) {
+        calculator.evaluator()
     }
 })
